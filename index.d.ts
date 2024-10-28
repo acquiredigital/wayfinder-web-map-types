401b6884ec1b7c3e5729968bed738e7870995ddd
Luke Nelson <luke@nelson.zone>
2024-10-28 15:41:49 +0000

    export as types

--- index.d.ts
+++ index.d.ts
@@ -1,25 +1,25 @@
 // Wayfinder class
-export * from "./types/Wayfinder";
+export type * from "./types/Wayfinder";
 
 // Top level exports
-export * from "./types/Bounds";
-export * from "./types/Database";
-export * from "./types/DestinationMesh";
-export * from "./types/Events";
-export * from "./types/Overlay";
-export * from "./types/Settings";
+export type * from "./types/Bounds";
+export type * from "./types/Database";
+export type * from "./types/DestinationMesh";
+export type * from "./types/Events";
+export type * from "./types/Overlay";
+export type { Settings } from "./types/Settings";
 
 // Database exports
-export * from "./types/database/Amenities";
-export * from "./types/database/AmenityTypes";
-export * from "./types/database/Building";
-export * from "./types/database/Destination";
-export * from "./types/database/DestinationCategory";
-export * from "./types/database/Floor";
-export * from "./types/database/MeshLinks";
-export * from "./types/database/Node";
-export * from "./types/database/TransitNodes";
-export * from "./types/database/TravelTypes";
+export type * from "./types/database/Amenities";
+export type * from "./types/database/AmenityTypes";
+export type * from "./types/database/Building";
+export type * from "./types/database/Destination";
+export type * from "./types/database/DestinationCategory";
+export type * from "./types/database/Floor";
+export type * from "./types/database/MeshLinks";
+export type * from "./types/database/Node";
+export type * from "./types/database/TransitNodes";
+export type * from "./types/database/TravelTypes";
 
 export as namespace WF;
 
